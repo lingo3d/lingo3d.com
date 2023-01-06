193eb8eb12b6b05e2e77ee23f882ec31f7bb3742
Xz33M Hask <pecevgligor@protonmail.com>
2023-01-06 16:06:44 +0100

    update

--- frontend/pages/api/image_upload.ts
+++ frontend/pages/api/image_upload.ts
@@ -5,7 +5,8 @@ import { NextApiRequest, NextApiResponse } from "next"
 cloudinary.v2.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true
 })
 
 export const config = {
@@ -18,25 +19,32 @@ export default async function upload(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
-    const data = await new Promise((resolve, reject) => {
-        const form = new formidable.IncomingForm()
-        form.parse(req, (err, fields, files) => {
-            if (err) return reject(err)
-            resolve({ fields, files })
+    if (req.method === "POST") {
+        const data = await new Promise((resolve, reject) => {
+            const form = new formidable.IncomingForm()
+            form.parse(req, (err, fields, files) => {
+                if (err) return reject(err)
+                resolve({ fields, files })
+            })
         })
-    })
-    const filePath = data?.files?.file.filepath
+        const filePath = data?.files?.file.filepath
+        const fileName = data.files.file.originalFilename
+        const upload_preset = data?.fields?.upload_preset
 
-    try {
-        const response = await cloudinary.v2.uploader.upload(filePath, {
-            public_id: req.cookies.id
-        })
-        res.json({
-            response: response
-            // file: data.files,
-            // fields: data.fields
-        })
-    } catch (err) {
-        return res.json({ err: err, message: "error" })
+        try {
+            const response = await cloudinary.v2.uploader.upload(filePath, {
+                public_id: req.cookies.id,
+                upload_preset: upload_preset,
+                use_filename: true,
+                unique_filename: false
+            })
+            res.json({
+                response: response
+            })
+        } catch (err) {
+            return res.json({ err: err, message: "error" })
+        }
+    } else {
+        return res.status(403).send("Forbidden")
     }
 }
