065c8edd4db38a7ce17d31ee0e6bc7838ec5c720
Xz33M Hask <pecevgligor@protonmail.com>
2023-01-24 08:23:27 +0100

    update

--- frontend/components/errors/500.tsx
+++ frontend/components/errors/500.tsx
@@ -1,8 +1,12 @@
 const Error500 = () => {
     return (
         <div className="w-full h-screen flex justify-center items-center">
-            <h1 className="textColor2 text-center text-3xl">500 - Server error</h1>
-            <h1 className="textColor2 text-center text-3xl">Please try again</h1>
+            <h1 className="textColor2 text-center text-3xl">
+                500 - Server error
+            </h1>
+            <h1 className="textColor2 text-center text-3xl">
+                Please try again{" "}
+            </h1>
         </div>
     )
 }
