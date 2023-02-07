f5816ae9a23f9b94122d0af530498181383333fa
Xz33M Hask <pecevgligor@protonmail.com>
2023-02-07 23:35:40 +0800

    add react hook form for user registration

--- frontend/components/login_register/Register.tsx
+++ frontend/components/login_register/Register.tsx
@@ -1,65 +1,156 @@
-import { useState } from "react"
+import { useState, useRef } from "react"
+import { useForm, Controller, SubmitHandler } from "react-hook-form"
 import TextField from "@mui/material/TextField"
 import Button from "@mui/material/Button"
 import { setToken } from "../../pages/api/auth/js-cookie"
 
-export default function SignUp() {
-    const [email, setEmail] = useState("")
-    const [username, setUsername] = useState("")
-    const [password, setPassword] = useState("")
-    const [passwordConfirm, setPasswordConfirm] = useState("")
+type Inputs = {
+    email: string
+    username: string
+    password: string
+    passwordConfirm: string
+}
 
-    const [passwordConfirmError, setPasswordConfirmError] = useState(false)
+export default function SignUp() {
     const [show, setShow] = useState(false)
     const [serverError, setServerError] = useState("")
 
-    async function registerUser() {
-        if (password !== passwordConfirm) setPasswordConfirmError(true)
+    const {
+        control,
+        handleSubmit,
+        watch,
+        formState: { errors }
+    } = useForm({
+        defaultValues: {
+            email: "",
+            username: "",
+            password: "",
+            passwordConfirm: ""
+        }
+    })
 
-        const res = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_URL}/api/auth/local/register`, {
-            headers: {
-                "Content-Type": "application/json"
-            },
-            method: "POST",
-            body: JSON.stringify({
-                password: password,
-                email: email,
-                username: username
-            })
-        }).catch((err) => console.log(err))
+    const password = useRef({})
+    password.current = watch("password", "")
+
+    const registerUser: SubmitHandler<Inputs> = async (data: Inputs) => {
+        const submitData = await fetch(
+            `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/auth/local/register`,
+            {
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                method: "POST",
+                body: JSON.stringify({
+                    password: data.password,
+                    email: data.email,
+                    username: data.username
+                })
+            }
+        ).catch((err) => console.log(err))
 
         //@ts-ignore
-        const data = await res.json()
+        const res = await submitData.json()
 
-        if (data.error) {
+        if (res.error) {
             setShow(true)
-            setServerError(data.error.message)
+            setServerError(res.error.message)
             return
         }
 
-        setToken(data)
+        setToken(res)
     }
 
     return (
         <>
             <div className="text-[22px]">Create your account</div>
             <div>Enter your desired username and password</div>
-            <TextField fullWidth required label="Email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-            <TextField fullWidth required label="Username" type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-            <TextField fullWidth required label="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            <TextField
-                fullWidth
-                required
-                label="Confirm password"
-                type="password"
-                value={passwordConfirm}
-                onChange={(e) => setPasswordConfirm(e.target.value)}
-                error={passwordConfirmError}
-                helperText={passwordConfirmError && `Password does not match`}
+            <Controller
+                control={control}
+                name="email"
+                rules={{ required: true }}
+                render={({ field: { onChange, value, ref } }) => (
+                    <TextField
+                        fullWidth
+                        inputRef={ref}
+                        label="Email"
+                        type="email"
+                        value={value}
+                        onChange={onChange}
+                        error={!!errors.username}
+                    />
+                )}
             />
-            {show ? <span className="w-full text-center text-red-900 border-[1px] border-red-900 rounded py-2">{serverError}</span> : null}
+            <Controller
+                control={control}
+                name="username"
+                rules={{ required: true }}
+                render={({ field: { onChange, value, ref } }) => (
+                    <TextField
+                        fullWidth
+                        inputRef={ref}
+                        label="Username"
+                        type="text"
+                        value={value}
+                        onChange={onChange}
+                        error={!!errors.username}
+                    />
+                )}
+            />
+            <Controller
+                control={control}
+                name="password"
+                rules={{ required: true, minLength: 6 }}
+                render={({ field: { onChange, value, ref } }) => (
+                    <TextField
+                        fullWidth
+                        inputRef={ref}
+                        label="Password"
+                        type="password"
+                        value={value}
+                        onChange={onChange}
+                        error={!!errors.password}
+                    />
+                )}
+            />
+            {errors.password && errors.password.type === "minLength" && (
+                <span>Minimum password length is 6 charachters</span>
+            )}
+            <Controller
+                control={control}
+                name="passwordConfirm"
+                rules={{
+                    required: true,
+                    minLength: 6,
+                    validate: (val: string) => {
+                        if (watch("password") != val) {
+                            return (
+                                val === password.current ||
+                                "Passwords do not match"
+                            )
+                        }
+                    }
+                }}
+                render={({ field: { onChange, value, ref } }) => (
+                    <TextField
+                        fullWidth
+                        inputRef={ref}
+                        label="Confirm password"
+                        type="password"
+                        value={value}
+                        onChange={onChange}
+                        error={!!errors.passwordConfirm}
+                    />
+                )}
+            />
+            {errors.passwordConfirm && <p>{errors.passwordConfirm.message}</p>}
+
+            {show ? (
+                <span className="w-full text-center text-red-900 border-[1px] border-red-900 rounded py-2">
+                    {serverError}
+                </span>
+            ) : null}
             <Button
-                onClick={() => registerUser()}
+                onClick={handleSubmit(registerUser)}
                 fullWidth
                 sx={{
                     background: "#293ab9",
