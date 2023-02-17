726dac0961bf58fc8080450d05a3fa87a8ff37f9
Xz33M Hask <pecevgligor@protonmail.com>
2023-02-17 11:52:07 +0800

    add initial motion animation

--- homepage_redesign/pages/index.tsx
+++ homepage_redesign/pages/index.tsx
@@ -1,13 +1,23 @@
 import Navigation from "@/layouts/Navigation/index"
 import VideoSection from "@/components/VideoSection"
 import CarouselSection from "@/components/CarouselSection"
+import { motion } from "framer-motion"
 
 export default function Home() {
     return (
         <main className="relative h-[4000px] bg-black">
-            <Navigation />
-            <VideoSection />
-            <CarouselSection />
+            <motion.div
+                initial={{ y: 80 }}
+                animate={{ y: 0 }}
+                transition={{
+                    duration: 1.3,
+                    delay: 0.2
+                }}
+            >
+                <Navigation />
+                <VideoSection />
+                <CarouselSection />
+            </motion.div>
         </main>
     )
 }
