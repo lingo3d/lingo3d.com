e8ab461e0ce1848db812b6490b33c1c276c8771f
Xz33M Hask <pecevgligor@protonmail.com>
2023-02-23 11:29:58 +0800

    decompomize footer

--- homepage_redesign/pages/index.tsx
+++ homepage_redesign/pages/index.tsx
@@ -6,6 +6,7 @@ import HeroTitle from "@/components/HeroTitle"
 import CarouselSection from "@/components/CarouselSection"
 import Parallax from "@/components/Parallax"
 import Footer from "@/layouts/footer"
+import ICP from "@/layouts/footer/ICP"
 
 const useScroll = () => {
     const [scrollY, setScrollY] = useState(0)
@@ -177,7 +178,35 @@ export default function Home() {
                 scrollNormalized={scrollNormalized}
                 setElVideo={setElVideo}
             />
-            <Footer ref={footerRef} />
+            <footer ref={footerRef} className="z-[999] w-full bg-gray-500">
+                <section className="w-full h-full flex justify-center items-center relative">
+                    <div className="upperColor absolute top-0 w-full h-[50%] bg-[#e5e5e5]" />
+                    <div className="bottomColor absolute bottom-0 w-full h-[50%] bg-[#434343]" />
+                    <div
+                        className="image-container w-full h-[470px] my-[115px] mx-[35px]
+                    md:h-[387px] 
+                    lg:mx-[90px] 
+                    xl:mx-[160px] 
+                    2xl:mx-[225px]   bg-[#969696] 2xl:h-[591px] z-[100]"
+                    >
+                        <picture className="w-full h-full">
+                            <source
+                                media="(min-width: 768px)"
+                                srcSet="/brands_horizontal.png"
+                                className="w-full h-full"
+                            />
+                            <img
+                                src="/brands_vertical.png"
+                                alt="Logo"
+                                className="w-full h-full"
+                            />
+                        </picture>
+                    </div>
+                </section>
+                <ICP />
+            </footer>
+
+            {/* <Footer ref={footerRef} /> */}
         </main>
     )
 }
