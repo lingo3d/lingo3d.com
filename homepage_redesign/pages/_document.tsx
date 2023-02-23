f6194e4b08c1ce65a842c7b28b5bd4fc31a43126
Xz33M Hask <pecevgligor@protonmail.com>
2023-02-23 15:17:21 +0800

    add custom font preload to prevent layout shift

--- homepage_redesign/pages/_document.tsx
+++ homepage_redesign/pages/_document.tsx
@@ -1,13 +1,41 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document"
+import React from "react"
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    const fontWeights = [100, 500]
+
+    return (
+        <Html lang="en">
+            <Head>
+                {fontWeights.map((weight) => (
+                    <React.Fragment key={weight}>
+                        <link
+                            key={`font-woff2-${weight}`}
+                            rel="preload"
+                            href={`/fonts/${weight}-graphie.woff2`}
+                            as="font"
+                            type="font/woff2"
+                            crossOrigin="anonymous"
+                        />
+                    </React.Fragment>
+                ))}
+                {fontWeights.map((weight) => (
+                    <React.Fragment key={weight}>
+                        <link
+                            key={`font-woff2-${weight}`}
+                            rel="preload"
+                            href={`/fonts/${weight}-graphie.woff`}
+                            as="font"
+                            type="font/woff2,font/woff"
+                            crossOrigin="anonymous"
+                        />
+                    </React.Fragment>
+                ))}
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
 }
