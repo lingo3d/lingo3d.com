6b5156a17d1ebe8af74b616e2db2131378841e66
Xz33M Hask <pecevgligor@protonmail.com>
2023-03-06 16:32:34 +0800

    add codesandbox react

---
+++ sandbox-R/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className="w-full h-full flex justify-center items-center bg-slate-800">
+            <p className="text-yellow-300">Click on the Vite and React logos to learn more</p>
+        </div>
+    )
+}
+
+export default App
