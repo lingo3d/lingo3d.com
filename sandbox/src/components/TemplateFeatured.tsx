e858d168f37a1f0c6b614c537a9d292a9c6ad4a6
Xz33M Hask <pecevgligor@protonmail.com>
2023-03-09 16:44:11 +0800

    rename Sandbox-R to Sandbox after removing the NextJS version

--- sandbox/src/components/TemplateFeatured.tsx
+++ sandbox/src/components/TemplateFeatured.tsx
@@ -5,7 +5,7 @@ import featured from "/featured.webp"
 const TemplateFeatured = () => {
     return (
         <Box className="bg-[#1f1e24] w-full cursor-pointer h-[210px] lg:h-[256px] rounded">
-            <CardMedia component="img" image={featured} alt="react icon" className="w-full h-full" />
+            <CardMedia component="img" image={featured} alt="react icon" className="w-full h-full rounded" />
         </Box>
     )
 }
